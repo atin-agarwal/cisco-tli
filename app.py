1e53cd2d8d797162e2bbea8726579b5e8711cf99
atin-agarwal <atin1723@gmail.com>
2019-12-06 14:20:22 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 #!flask/bin/python
 from flask import Flask,request
-import os
+import pymongo
+import subprocess
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["test"]
+mycol = mydb["model_inventory"]
+
 
 app = Flask(__name__)
 
@@ -10,17 +16,28 @@ def index():
 
 @app.route('/api/v1/tryit', methods=['GET'])
 def try_it():
-    api_id = request.args.get('apiId')
-    param1 = request.args.get('inp1')
-    param2 = request.args.get('inp2')
-    param3 = request.args.get('inp3')
-    print('API requested for :: ' + api_id)
-    print('Input Set is :: ', param1, param2, param3)
-    
-    cmd = 'python3 <PATH_TO_PY_MODEL_SCRIPT> ' + param1 + ' ' + param2 + ' ' + param3
-    print('Model running :: ', cmd)
-    os.system(cmd)
-    return ("Done...Please check console for output...")
+    api_name = request.args.get('apiName')
+    print('API Name :: ' + api_name)
+
+    myquery = { "name": api_name }
+    myDoc = mycol.find_one(myquery)
+
+    result = myDoc
+    print("Result from DB :: ", result)
+
+    modelFileLocation = result['modelFileLocation']
+    inputFileLocation = result['inputFileLocation']
+
+    cmd = 'python3 main.py ' + modelFileLocation + ' ' + inputFileLocation
+    print('Command running :: ', cmd)
+
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+    out, err = p.communicate() 
+    decodedOut = out.decode('utf8')
+    print("Err:: ", err)
+    print("Output :: " , decodedOut)
+
+    return (decodedOut)
    
 
 if __name__ == '__main__':
